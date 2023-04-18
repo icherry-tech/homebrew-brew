87b9ea7516632e6d6278c99f688f7601f30080e0
黄咏玉 <hylm@me.com>
2023-04-18 12:14:16 +0800

    4.18

--- Formula/che.rb
+++ Formula/che.rb
@@ -5,7 +5,7 @@ class Che < Formula
   desc ""
   homepage ""
   url "https://github.com/icherry-tech/che/releases/download/v1.1/che.tar.gz"
-  sha256 "5e973bc1e20b2416d2b8a66d0aeddaaa1dfe0b0d48a80f8afca7f100c6e3d42c"
+  sha256 "8bb587f8a429b565a40dd23e30e990be663cfaae0be17fde6f74d95476d69786"
   license ""
   version "v1.1"
 
