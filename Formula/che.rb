37520d2aeb8deae7a0b67a0160cf08691e9d2a10
黄咏玉 <hylm@me.com>
2023-04-18 13:47:18 +0800

    aaa

--- Formula/che.rb
+++ Formula/che.rb
@@ -4,10 +4,10 @@
 class Che < Formula
   desc ""
   homepage ""
-  url "https://github.com/icherry-tech/che/releases/download/1.9/che.tar.gz"
-  sha256 "cfaaae75d1413abb1f514303ac7d53e04b1871c1ed47b6ff0cc3937033d295d8"
+  url "https://github.com/icherry-tech/che/releases/download/2.0/che.tar.gz"
+  sha256 "cd1aca94e8ead68e63e3f88dbd7b5e32c976f606f4e6b2b39dff8a2c35c9f3c5"
   license ""
-  version "1.9"
+  version "2.0"
 
   # depends_on "cmake" => :build
 
